482a1b0b318aebdc1ab7ed91030663042d80eb90
vysakh P Pillai <vysakhpillai@gmail.com>
2018-03-09 15:50:35 +0530

    adding more SSI registration stuff

--- include/http_SSI_functionRegistration.h
+++ include/http_SSI_functionRegistration.h
@@ -7,13 +7,26 @@
 When a registered SSI string is found in the page, the associated callback will be called to 
 provide the dynamic replacement contents */
 
-typedef char* (*http_SSI_replacer_cb) (const char *SSIString);
+typedef char *(*http_SSI_replacer_cb)(const char *SSIString);
 
+/*The below structure holds the pointer to a static string to be replaced and 
+  the corresponding replacer function that returns the replacement string. 
+*/
+typedef struct{
+    const char* SSI_String;
+    http_SSI_replacer_cb SSI_stringReplacer;
+    unsigned int SSI_replacer_arrayIndex;
+}http_SSI_replacer_t;
+
+typedef http_SSI_replacer_t* http_SSI_replacerHandle_t
 /*function used to register a SSI string replacer function. 
     IN : string to be replaced and associated callback function
     OUT : 0  = Success
         : -1 = Failure
 */
-int http_SSI_registerReplacer(const char* SSIString,http_SSI_replacer_cb SSIReplacerCb);
+http_SSI_replacerHandle_t http_SSI_register_replacer(const char *SSIString, http_SSI_replacer_cb SSIReplacerCb);
+
+/*function to deRegister a SSI replacement function*/
+int http_SSI_deRegister_replacer(http_SSI_replacerHandle_t deregisterHandle);
 
 #endif
--- src/http_SSI_functionRegistration.c
+++ src/http_SSI_functionRegistration.c
@@ -3,9 +3,16 @@
 #include "http_config.h"
 #include "http_common.h"
 
+http_SSI_replacer_t http_SSI_replacer[HTTP_MAX_SSI_SIZE];
+static int http_SSI_replacerIndex;  //store current number of registrations
 
-
-int http_SSI_registerReplacer(const char* SSIString,http_SSI_replacer_cb SSIReplacerCb)
+http_SSI_replacerHandle_t http_SSI_register_replacer(const char *SSIString, http_SSI_replacer_cb SSIReplacerCb)
 {
 
+}
+
+int http_SSI_deRegister_replacer(http_SSI_replacerHandle_t deregisterHandle)
+{
+    //free the stuff?
+    //make contents in array 0
 }
